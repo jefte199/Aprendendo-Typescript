dde41e89b93ea93298e12a1728f58f37dc50f38a
Jefte <jefte199@gmail.com>
2023-01-07 13:41:05 -0300

    feat: add exercicio

---
+++ exerciciosTS/type_guard.js
@@ -0,0 +1,17 @@
+"use strict";
+const arr = [{ key: 0 }, { key: 1 }, { key: 2 }];
+function isString(value) {
+    if (typeof value === "string") {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+if (isString(arr)) {
+    console.log("string");
+}
+else {
+    console.log("no string");
+}
+console.log(arr instanceof Array);
